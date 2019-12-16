0f50c0baf172169cd795e9affbf84d39ff49a53b
yujiaxue0129 <2422700101@qq.com>
2019-12-16 23:49:02 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,37 +1,57 @@
 <template>
   <div>
-    <i-grid>
-    <i-grid-item>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/images/1.png" />
         </i-grid-icon>
         <i-grid-label>消息</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/images/2.png" />
         </i-grid-icon>
         <i-grid-label>科目一</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/images/3.png" />
         </i-grid-icon>
         <i-grid-label>科目二</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/images/4.png" />
         </i-grid-icon>
         <i-grid-label>科目三</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/images/5.png" />
         </i-grid-icon>
         <i-grid-label>科目四</i-grid-label>
     </i-grid-item>
   </i-grid>
+  <i-panel title="最新消息">
+    <view style="padding: 15px;">
+      <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+       <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+       <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+       <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+    </view>
+  </i-panel>
   </div>
 </template>
 
@@ -52,5 +72,10 @@ export default {
 </script>
 
 <style scoped>
-
+div >>> .no-border {
+  border-width: 0pt;
+}
+div >>> .split {
+  margin-bottom: 10pt;
+}
 </style>
